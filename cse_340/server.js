f07841a327d03d2dfaa0714455fd1f5fdda7838d
laura nunez <laupao41@gmail.com>
2025-07-31 18:26:57 +0900

    initial commit

--- cse_340/server.js
+++ cse_340/server.js
@@ -10,56 +10,27 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
 
-
-/* ******************************************
+/* ***********************
  * View Engine and Templates
- * ***************************************** */
-
-/* ******************************************
- * View Engine and Templates
- * ***************************************** */
+ *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
 
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-// Index route
-app.get("/", baseController.buildHome)
-// Inventory routes
-app.use("/inv", inventoryRoute)
-
-// File Not Found Route - must be last route in list
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
-})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500
-const host = process.env.HOST || 'localhost'
-
-
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message: err.message,
-    nav
-  })
-})
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
