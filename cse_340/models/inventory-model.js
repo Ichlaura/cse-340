cd9c614f82c1aa932e11e7d70bcad4c4f29c65ff
laura nunez <laupao41@gmail.com>
2025-08-02 07:52:43 +0900

    initial commit

--- cse_340/models/inventory-model.js
+++ cse_340/models/inventory-model.js
@@ -10,21 +10,17 @@ async function getClassifications(){
 module.exports = {getClassifications}
 
 
-
-/* ***************************
- *  Get all inventory items and classification_name by classification_id
- * ************************** */
-async function getInventoryByClassificationId(classification_id) {
-  try {
+async function getClassifications() {
     const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`,
-      [classification_id]
+      "SELECT * FROM public.classification ORDER BY classification_name"
     )
     return data.rows
-  } catch (error) {
-    console.error("getclassificationsbyid error " + error)
   }
-}
+
+  async function getClassificationById(classification_id) {
+    const data = await pool.query(
+      "SELECT * FROM public.classification WHERE classification_id = $1 ORDER BY classification_name",
+       [classification_id]
+    )
+    return data.rows[0]
+  }
