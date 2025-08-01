c5bd5b11d5df6bf6571fc48501e9b3713c18b506
laura nunez <laupao41@gmail.com>
2025-08-02 08:06:42 +0900

    initial commit

--- cse_340/models/inventory-model.js
+++ cse_340/models/inventory-model.js
@@ -3,24 +3,41 @@ const pool = require("../database/")
 /* ***************************
  *  Get all classification data
  * ************************** */
-async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+async function getClassifications() {
+  const data = await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+  )
+  return data.rows
 }
 
-module.exports = {getClassifications}
+async function getClassificationById(classification_id) {
+  const data = await pool.query(
+    "SELECT * FROM public.classification WHERE classification_id = $1 ORDER BY classification_name",
+    [classification_id]
+  )
+  return data.rows[0]
+}
 
-
-async function getClassifications() {
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
     const data = await pool.query(
-      "SELECT * FROM public.classification ORDER BY classification_name"
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id]
     )
     return data.rows
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
   }
+}
 
-  async function getClassificationById(classification_id) {
-    const data = await pool.query(
-      "SELECT * FROM public.classification WHERE classification_id = $1 ORDER BY classification_name",
-       [classification_id]
-    )
-    return data.rows[0]
-  }
+module.exports = {
+  getClassifications,
+  getClassificationById,
+  getInventoryByClassificationId
+}
