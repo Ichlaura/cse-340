30415cfdaad514b40815931c8ddf15cdc4c4ace9
laura nunez <laupao41@gmail.com>
2025-08-07 18:44:58 +0900

    week4_Account: Process Registration_errors

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -19,5 +19,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 //unit 4 
 //Deliver Registration View
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// Process the registration form
+router.post("/register", utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router
